c230cebe55c332ce76ad62900b7372b4d821ced1
Miguel Rodriguez Alvarez <miguel7.254@gmail.com>
2022-04-11 13:01:15 +0200

    all draw and delete events implemented

--- angular-shared-whiteboard/src/app/canvas/canvas.component.ts
+++ angular-shared-whiteboard/src/app/canvas/canvas.component.ts
@@ -23,12 +23,12 @@ export class CanvasComponent implements OnInit {
       this.emite = false;
       this.idShape = res.name;
       console.log('subscribe', res, this.idShape)
-      this.draw(res)
+      this.drawFromEvent(res)
     });
     this.socketService.callRemove.subscribe((res: any) => {
       this.emite = false;
       console.log(res)
-      this.delete(res)
+      this.deleteFromEvent(res)
     });
   }
   ngOnInit(): void {
@@ -40,7 +40,7 @@ export class CanvasComponent implements OnInit {
           this.socketService.drawEvent(e, e.target?.name?.toString());
         } else {
           if (e != null && e.target != null) {
-            let name =this.generateId();
+            let name =this.generateObjectId();
             if(name!=null){
               e.target.name = name;
               this.socketService.drawEvent(e, name);
@@ -59,7 +59,7 @@ export class CanvasComponent implements OnInit {
     });
  
   }
-  delete(nameToRemove: string): void {
+  deleteFromEvent(nameToRemove: string): void {
     if (nameToRemove != null) {
       this.canvas.getObjects().forEach(shape => {
         console.log(shape)
@@ -70,22 +70,21 @@ export class CanvasComponent implements OnInit {
     }
   }
 
-  draw(e: any): void {
+  drawFromEvent(e: any): void {
     if (e != null) {
       var target = e.res.target;
-      console.log(e.res.target.type)
+      var newTarget = this.addObjectIdToTargetOptions(target,e.name);
       switch (target.type) {
         case "rect":
           this.canvas.add(new fabric.Rect({
             width: target?.width, height: target?.height, left: target?.left, top: target?.top, fill: target?.fill, name: e.name
           }))
-          console.log(this.canvas.getObjects())
           break;
         case "circle":
           this.canvas.add(new fabric.Circle({ width: target?.width, height: target?.height, left: target?.left, top: target?.top, fill: target?.fill, name: e.name, angle: 45, radius: 50 }));
           break;
         case "textbox":
-          this.canvas.add(new fabric.Textbox('', e.target));
+          this.canvas.add(new fabric.Textbox('', newTarget));
           break;
         case "path":
           var pathValue = this.mapPathFromEvent(e);
@@ -102,6 +101,12 @@ export class CanvasComponent implements OnInit {
       }
     }
   }
+  addObjectIdToTargetOptions(target: any, objectId:string): fabric.IObjectOptions {
+    var ret = target as fabric.IObjectOptions;
+    console.log("name from method", objectId);
+    ret.name = objectId;
+    return ret;
+  }
   mapPathFromEvent(e: any): string {
     console.log(e)
     const path = e.res.target.path as Array<Array<any>>;
@@ -114,49 +119,48 @@ export class CanvasComponent implements OnInit {
     return pathValue;
   }
 
-  drawText(): void {
+  onClick_drawTextButton(): void {
     this.emite = true;
     this.canvas.isDrawingMode = false;
     this.canvas.add(new fabric.Textbox('Insert text'));
   }
-  freeDraw(): void {
+  onClick_freeDrawButton(): void {
     this.emite = true;
     this.canvas.isDrawingMode = !this.canvas.isDrawingMode;
   }
-  drawCircle(): void {
+  onClick_drawCircleButton(): void {
     this.emite = true;
     this.canvas.add(new fabric.Circle({
-      width: 100, height: 100, left: 100, top: 100, fill: '#725AC1', angle: 45, radius: 50, name: this.generateId()
+      width: 100, height: 100, left: 100, top: 100, fill: '#725AC1', angle: 45, radius: 50, name: this.generateObjectId()
     }))
   }
-
-  drawRect(): void {
+  onClick_drawRectButton(): void {
     this.emite = true;
     this.canvas.add(new fabric.Rect({
-      width: 100, height: 100, left: 100, top: 100, fill: '#725AC1', name: this.generateId()
+      width: 100, height: 100, left: 100, top: 100, fill: '#725AC1', name: this.generateObjectId()
     }))
     console.log(this.canvas.getObjects())
 
   }
-  generateId(): string | undefined {
-    return Math.floor((Math.random() * 999999) + 1).toString();
-  }
-  deleteBtn(): void {
+  onClick_deleteButton(): void {
     this.emite = true;
     var selectedObjects = this.canvas.getActiveObjects();
     selectedObjects.forEach(shape => {
       this.removeShape(shape);
     });;
   }
-  removeShape(s: fabric.Object) {
-    if (s == null) return;
-    this.canvas.remove(s);
-  }
-  changeColor(): void {
+  onClick_changeColor(): void {
     var activeObjects = this.canvas.getActiveObjects();
     activeObjects.forEach(o => {
       o.set('fill', 'blue')
     });;
   }
-
+  removeShape(s: fabric.Object) {
+    if (s == null) return;
+    this.canvas.remove(s);
+  }
+ 
+  generateObjectId(): string | undefined {
+    return Math.floor((Math.random() * 999999) + 1).toString();
+  }
 }
