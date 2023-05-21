c4085d0f767335afc535188d23b5b6477b9ea560
Miguel Rodriguez Alvarez <miguel7.254@gmail.com>
2023-05-21 21:45:29 +0200

    testing pixels

--- angular-shared-whiteboard/src/app/test.js
+++ angular-shared-whiteboard/src/app/test.js
@@ -1,10 +1,8 @@
-const { fabric } = require('fabric');
-const { Builder, By, until, WebElementCondition } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const chromedriver = require('chromedriver');
+const { AssertionError } = require('assert');
+const { Builder, By, until} = require('selenium-webdriver');
 describe('testing', () => {
     try {
-        jest.setTimeout(60000); //By default jest tests are at least 5 seconds.
+        jest.setTimeout(500000); //By default jest tests are at least 5 seconds.
 
         test('it should draw a rectangle', async () => {
 
@@ -14,74 +12,53 @@ describe('testing', () => {
             let driver2 = new Builder().forBrowser('chrome').build();
             await driver2.get('http://localhost:4200/sala1');
 
+            await driver1.wait(until.elementLocated(By.id('canvas')), 60000);
+            await driver2.wait(until.elementLocated(By.id('canvas')), 60000);
+
+            var canvas1Data = await driver1.executeScript('return document.getElementById("canvas").getContext("2d").getImageData(0,0,400,400);').then(async e => {
+                return e
+            });
+            console.log('canvas1Data ', canvas1Data.data);
+            var canvas2Data = await driver2.executeScript('return document.getElementById("canvas").getContext("2d").getImageData(0,0,400,400);').then(async e => {
+                return e
+            });
+            console.log('canvas2Data ', canvas2Data.data);
+
+            canvas1Data.data.forEach(function(pixel) {
+                return pixel==expect(0);
+            });
+            canvas2Data.data.forEach(function(pixel) {
+                return pixel==expect(0);
+            });
+
             let btnShapes = await driver2.findElement(By.id('btnshapes'));
             await btnShapes.click();
 
             let btnShapeRect = await driver2.findElement(By.id('btnRect'));
-            await btnShapeRect.click();
-
-
-
-            // jest.setTimeout(60000);
-
-            // await driver1.manage().setTimeouts(( { implicit: 60000 } ));
-            // driver1.sleep(60000);
-            // jest.setTimeout(60000);
-
-
-
-
-            // var canvas1 =  driver1.executeScript(' canvas');
-            // console.log('canvas1 ', canvas1);9
-            // canvas1.getAttribute('innerHTML').then(e=>{
-            //     console.log('html1 ', e);
-            // })
-            let ele = await driver1.wait(until.elementLocated(By.id('canvas')), 60000);
-            // console.log('ele',ele);
-            // let canvas = await driver1.findElement(By.id("canvas"));
-            // console.log(ele.getText());
-            // var el= driver1.findElements(By.id("canvas")).then(async e=>{
-            //     var canvsa = await driver1.findElement(By.id('canvas'));
-            //     console.log('canvsa',canvsa);
-            // });
-            //     console.log(el.getText())
-
-            var canvas = await driver1.executeScript('return document.getElementById("canvas").getContext("2d").getImageData(20,20,20,20);;').then(async e => {
-                // e[1].click()
-                // var att = e[1].getAttribute("innerHTML").then(async e => {
-                //     return e;
-                // });
-                // console.log('atri ', att);
-                return e
+            await btnShapeRect.click().then(async clicked=>{
+                var cv1data = await driver1.executeScript('return document.getElementById("canvas").getContext("2d").getImageData(0,0,400,400).data;').then(async e => {
+                    return e
+                });
+                console.log('cv1data drawed: ', cv1data);
+                cv1data.forEach(function(pixel) {
+                    return pixel==expect(0);
+                });
+                var cv2data = await driver2.executeScript('return document.getElementById("canvas").getContext("2d").getImageData(0,0,400,400).data;').then(async e => {
+                    return e
+                });
+                console.log('cv2data drawed: ', cv2data);
+                let sonCeros = cv2data.forEach(function(pixel) {
+                    return pixel==0;
+                });
+                console.log(sonCeros)
             });
-            console.log('canvas ', canvas);
-            // var attr = await canvas12.getAttribute("innerHTML").then(e=>{
-            //     return e;
-            // })
-            // console.log('attr '. attr);
-            // const element = driver1.findElement(By.id('canvas'));
-            // console.log(await element.getAttribute('innerHTML').then(e=>{
-            //     console.log('que te pintes coño ', e)
-            // }));
-            // let cv =  driver1.executeScript('let canvas = document.getElementById("canvas");');
-            // console.log('dscsdsdv ', cv);
-            // cv.then(async e=>{
-            //     await console.log('sdfsdf ', e);
-            // })
+
+          
+         
 
 
-            // let canvas = await driver1.findElement(By.xpath('canvas[id*="canvas"]'));
-            // let html = await canvas.getAttribute('innerHTML').then(e => {
-            //     console.log(e);
-            // });
-            // console.log(html);
-            // var c = p.then(function(e){
-            //     console.log(e);
-            // });
-            // var bg = c.toDataURL("image/png");
-
-            // var canvas = new fabric.Canvas('c');
-            // console.log(canvas);
+            driver1.close();
+            driver2.close();
         });
     } catch {
         console.log()
